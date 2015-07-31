12e95b636fc7473396065ed2898c6558db9e6fc2
cameronaverill <cameronaverill2018@u.northwestern.edu>
2015-07-31 09:36:46 +0200

    heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: ['welcome#index']
 
   def check_user
   	if current_user != Photo.find(params[:id]).user
--- config/routes.rb
+++ config/routes.rb
@@ -23,11 +23,11 @@ Rails.application.routes.draw do
   end
 
   
-
+  resources :welcome, only: [:index]
 
    # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  root 'photos#index'
+  root 'welcome#index'
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
